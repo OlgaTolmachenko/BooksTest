7331096d5ab19af9cf39ca4fee4a5fbf785e4726
Olga Tolmachenko <tolmachenko.olga@gmail.com>
2016-12-02 11:59:28 +0200

    Search implemented

---
+++ app/src/main/java/com/example/tolmachenko/bookstest/BooksApiInterface.java
@@ -0,0 +1,8 @@
+package com.example.tolmachenko.bookstest;
+
+/**
+ * Created by Olga Tolmachenko on 02.12.16.
+ */
+
+public interface BooksApiInterface {
+}
--- app/src/main/java/com/example/tolmachenko/bookstest/MainActivity.java
+++ app/src/main/java/com/example/tolmachenko/bookstest/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.tolmachenko.bookstest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        searchView.setSearchableInfo(searchableInfo);
+        searchView.setIconified(true);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                sharedPrefs = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                sharedPrefs.edit().putString(SearchManager.QUERY, query).apply();
+                searchView.clearFocus();
+                Log.d("MainActivity", "onQueryTextSubmit: " + query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d("MainActivity", "handleIntent: onQueryTextChange" + newText);
+                return false;
+            }
+        });
+
+        searchView.getQuery();
+
+        return true;
+    }
+
 }
